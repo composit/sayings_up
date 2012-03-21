bf59b1e5b6df8412872634cbb7bc5d160fcaa3de
Mr. Matt <system9@composition9.com>
2012-03-21 14:51:11 -0600

    wip: user sessions

--- spec/requests/manage_my_account_spec.rb
+++ spec/requests/manage_my_account_spec.rb
@@ -6,24 +6,27 @@ describe 'manage my account', %q{
   I want to create and manage my account
 }, :js do
 
-  context 'create an account' do
-    describe 'allows me to create an account' do
-      before :each do
-        visit '/'
-        click_link 'Sign up'
-        fill_in 'Username', with: 'Testuser'
-        fill_in 'Password', with: 'testpass'
-        fill_in 'Password confirmation', with: 'testpass'
-        click_button 'Create user'
-      end
+  it 'creates an account' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'Username', with: 'Testuser'
+    fill_in 'Password', with: 'testpass'
+    fill_in 'Password confirmation', with: 'testpass'
+    click_button 'Create user'
+    page.should have_content 'Welcome, Testuser'
+  end
 
-      it "notifies me of my account creation" do
-        page.should have_content 'Thanks for signing up!'
-      end
+  it 'logs in' do
+    Factory( :user, username: 'testuser', password: 'testpass', password_confirmation: 'testpass' )
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'Username', with: 'testuser'
+    fill_in 'Password', with: 'testpass'
+    click_button 'Sign in'
+    page.should have_content 'Welcome back, testuser'
+  end
 
-      it "signs me in to my account" do
-        page.should have_content 'Log out'
-      end
-    end
+  context 'log out' do
+    it 'allows me to log out if I am logged in'
   end
 end
