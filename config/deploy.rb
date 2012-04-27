0ef6f6d5f65c46123c4dac4a47e2bf0464d8a849
Mr. Matt <system9@composition9.com>
2012-04-27 12:34:36 -0600

    restart server script

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,18 +22,19 @@ server 'parasites', :app, :web, :db, primary: true
 # these http://github.com/rails/irs_process_scripts
 
 #If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-end
+#namespace :deploy do
+#  task :start do ; end
+#  task :stop do ; end
+#  task :restart, :roles => :app, :except => { :no_release => true } do
+#    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#  end
+#end
 
 after 'deploy:update_code' do
   run "ln -nfs #{deploy_to}/shared/config/mongoid.yml #{release_path}/config/mongoid.yml"
-  run "ln -nfs #{deploy_to}/shared/start_server.sh #{release_path}/start_server.sh"
+  run "ln -nfs #{deploy_to}/shared/restart_server.sh #{release_path}/restart_server.sh"
   #run "ln -nfs #{deploy_to}/shared/log/ #{release_path}/log/"
-  run "ln -nfs #{deploy_to}/shared/tmp #{release_path}/tmp"
+  run "ln -nfs #{deploy_to}/shared/tmp/cache #{release_path}/tmp/cache"
   run "cd #{release_path} && bundle install --without test --without development"
+  run "cd #{release_path} && ./restart_server.sh"
 end
