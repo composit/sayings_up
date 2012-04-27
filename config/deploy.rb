bac48c7a8e96e48b0a6d290bd4ecf06aecfbc006
Mr. Matt <system9@composition9.com>
2012-04-27 11:49:31 -0600

    update ruby point release

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :default_stage, 'staging'
 
 require 'rvm/capistrano'
 set :rvm_type, :system
-set :rvm_ruby_string, '1.9.3-p125'
+set :rvm_ruby_string, '1.9.3-p194'
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
