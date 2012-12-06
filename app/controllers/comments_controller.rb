20ea19d3012fb18058dea1f117f4adf180f430e0
Mr. Matt <matt@composition9.com>
2012-12-06 13:38:33 -0700

    clean up comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,18 +10,4 @@ class CommentsController < ApplicationController
     @exchange.save
     respond_with @exchange, @entry, @comment
   end
-=begin
-  load_and_authorize_resource
-
-  def create
-    exchange = Exchange.find( params[:exchange_id] )
-    @comment.entry = exchange.entries.find( params[:entry_id] )
-    @comment.user = current_user
-    if( @comment.save )
-      redirect_to exchange, notice: "Comment was successfully created"
-    else
-      redirect_to exchange, alert: "There was an error creating your comment"
-    end
-  end
-=end
 end
