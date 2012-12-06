277b34c4b780ad3c02b64bf7aa703827a95df441
Mr. Matt <matt@composition9.com>
2012-12-06 13:23:04 -0700

    clean up exchange

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -64,40 +64,4 @@ class Exchange
         entry.created_at
       end.collect( &:user ).compact.uniq[0..1]
     end
-=begin
-  field :parent_comment_id
-  field :parent_entry_id
-  field :parent_exchange_id
-  field :most_recent_entry_date, type: Time, default: Time.now
-
-  index :user_ids
-
-  embeds_many :entries
-  belongs_to_related :parent_exchange, class_name: "Exchange"
-
-  before_validation :set_user_ids
-
-  named_scope :top_level, where: { parent_comment_id: nil, parent_entry_id: nil, parent_exchange_id: nil }
-
-  def users
-    @users ||= User.where( :_id.in => user_ids ).to_a
-  end
-
-  def entries_attributes=( attributes_array )
-    # until mongoid supports accepts_nested_attributes_for fully
-    attributes_array.each do |entry_attributes|
-      entry = entries.create( entry_attributes )
-      entry.save
-    end
-  end
-
-  def ordered_entries
-    entries.sort { |x,y| x.created_at <=> y.created_at }
-  end
-
-  protected
-    def set_user_ids
-      self.user_ids = users.uniq.compact.collect { |user| user.id }
-    end
-=end
 end
