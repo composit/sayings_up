b9b5e790b31e186927d8ea3d324ea641120c1698
Mr. Matt <matt@composition9.com>
2012-12-06 13:37:02 -0700

    clean up application controller spec

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,8 +1,26 @@
 describe ApplicationController do
-  it 'sets the current user via the session' do
-    user = stub
+  let( :user ) { stub }
+
+  before :each do
     User.stub( :find ).with( 123 ) { user }
     session[:user_id] = 123
-    subject.send( :current_user ).should == user
+  end
+
+  it 'sets the current user via the session' do
+    expect( subject.send :current_user ).to eq user
+  end
+
+  it 'stores stores the current user rather than continuing to find via the database', :focus do
+    bad_user = stub
+    User.stub( :find ).with( 234 ) { bad_user }
+    session[:user_id] = 123
+    expect( subject.send :current_user ).to eq user
+    session[:user_id] = 234
+    expect( subject.send :current_user ).to eq user
+  end
+
+  it 'returns nil if there is no user id' do
+    session[:user_id] = nil
+    expect( subject.send :current_user ).to be_nil
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,38 +1,38 @@
 require 'spec_helper'
 
 describe User do
-  specify { build( :user ).should be_valid }
+  specify { expect( build :user ).to be_valid }
 
   it "only includes the id and username attributes in the json" do
     user = build :user
-    user.to_json.should =~ /^{\"_id\":\"\w+\",\"username\":\"\w+\"}$/
+    expect( user.to_json ).to match /^{\"_id\":\"\w+\",\"username\":\"\w+\"}$/
   end
 
   context "when validating" do
     it "requires a username" do
       user = build( :user, username: nil )
-      user.should_not be_valid
-      user.errors.messages.should == { username: ["can't be blank"] }
+      expect( user ).not_to be_valid
+      expect( user.errors.messages ).to eq( { username: ["can't be blank"] } )
     end
 
     it "requires a unique username" do
       create( :user, username: "testuser" )
       user = build( :user, username: "testuser" )
-      user.should_not be_valid
-      user.errors.messages.should == { username: ["is already taken"] }
+      expect( user ).not_to be_valid
+      expect( user.errors.messages ).to eq( { username: ["is already taken"] } )
     end
 
     it "requires a password" do
       user = build( :user, password: "", password_confirmation: "" )
-      user.should_not be_valid
-      user.errors.messages.should == { password_digest: ["can't be blank"] }
+      expect( user ).not_to be_valid
+      expect( user.errors.messages ).to eq( { password_digest: ["can't be blank"] } )
     end
   end
 
   context 'authenticating' do
     let( :user ) { build :user, password: 'testpass' }
 
-    specify { user.authenticate( 'testpass' ).should == user }
-    specify { user.authenticate( 'otherpass' ).should be_false }
+    specify { expect( user.authenticate 'testpass' ).to eq user }
+    specify { expect( user.authenticate 'otherpass' ).to be_false }
   end
 end
