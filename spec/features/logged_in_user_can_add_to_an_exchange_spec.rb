40c8528fe7f6bcffc51efd5cc520d1184641a8f5
Mr. Matt <matt@composition9.com>
2013-01-07 22:29:00 -0700

    user cannot respond to other users' entries feature

--- spec/features/logged_in_user_can_add_to_an_exchange_spec.rb
+++ spec/features/logged_in_user_can_add_to_an_exchange_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'logged in user can add to an exchange', :js do
   given( :exchange ) { create :exchange }
   given( :user ) { create :user, username: 'testuser' }
-  given( :entry ) { build( :entry, user: user, content: 'Go tell it on the mountain' ) }
+  given( :entry ) { build :entry, user: user, content: 'Go tell it on the mountain' }
 
   background do
     exchange.entries << entry
@@ -21,8 +21,7 @@ feature 'logged in user can add to an exchange', :js do
       click_button 'Add comment'
     end
     expect( page ).to have_content '1 comment'
-    pattern = Regexp.new( "new comment(.*)testuser", Regexp::MULTILINE )
-    expect( find( '.comments' ).text ).to match pattern
+    expect( find( '.comments' ).text ).to match /new comment(.*)testuser/m
   end
 
   scenario 'user can add an entry to an exchange he is involved in' do
@@ -44,11 +43,14 @@ feature 'logged in user can add to an exchange', :js do
       fill_in 'content', with: 'test response'
       click_button 'Respond'
     end
-    pattern = Regexp.new( "test response(.*)testuser", Regexp::MULTILINE )
-    expect( page ).to have_content pattern
+    expect( page ).to have_content /test response(.*)testuser/m
   end
 
-  scenario 'user cannot add an entry to an exchange she is not involved in'
+  scenario 'user cannot add an entry to an exchange she is not involved in' do
+    sign_in_as create :user
+    visit "/#e/#{exchange.id}"
+    expect( page ).to have_no_content 'respond'
+  end
 
   scenario 'user can respond to comments on one of her entries' do
     sign_in_as user
@@ -66,7 +68,19 @@ feature 'logged in user can add to an exchange', :js do
     expect( page ).to have_content 'test response'
   end
 
-  scenario 'user cannot respond to comments on another user\'s entries'
+  scenario 'user cannot respond to comments on another user\'s entries' do
+    other_user = create :user
+    other_entry = build :entry, user: other_user
+    other_entry.comments << build( :comment )
+    exchange.entries << other_entry
+    exchange.save!
+    sign_in_as user
+    visit "/#e/#{exchange.id}"
+    click_link '1 comment'
+    within( '.comments' ) do
+      expect( page ).to have_no_content 'respond'
+    end
+  end
 
   scenario 'user sees respond links immediately upon login' do
     visit "/#e/#{exchange.id}"
