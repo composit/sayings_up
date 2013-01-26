0372d184cb7150cf658db0ff47ba92d69cdc631f
Mr. Matt <matt@composition9.com>
2013-01-25 17:11:46 -0700

    secure secret token

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+CONFIG = YAML.load( File.read( File.expand_path( '../application.yml', __FILE__ ) ) )
+
 module Sayings
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Sayings::Application.config.secret_token = '5f467a363a7998e417851c305fdca8398667539e7c32f511ccae43ddfd83b6411a1f36896d9c1b9d14febe8fe475853bc4970a960e7f3bc9cf1458a3400b5772'
+Sayings::Application.config.secret_token = CONFIG[:secret_token]
