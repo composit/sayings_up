a6ce4371afb562f9259fe64f7a803645636f22eb
Mr. Matt <system9@composition9.com>
2013-09-26 14:36:34 -0600

    remove guard/spork in favor of zeus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,73 +1,60 @@
 require 'rubygems'
-require 'spork'
-#uncomment the following line to use spork with the debugger
-#require 'spork/ext/ruby-debug'
 
-Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However,
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+#require 'rspec/autorun'
+#require 'remarkable/active_model'
+#require 'remarkable/mongoid'
+#require 'factory_girl'
 
-  # This file is copied to spec/ when you run 'rails generate rspec:install'
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
-  #require 'rspec/autorun'
-  #require 'remarkable/active_model'
-  #require 'remarkable/mongoid'
-  #require 'factory_girl'
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
-  # Requires supporting ruby files with custom matchers and macros, etc,
-  # in spec/support/ and its subdirectories.
-  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+include Features::ExchangeHelpers
+include Features::SessionHelpers
+include SessionHelpers
 
-  include Features::ExchangeHelpers
-  include Features::SessionHelpers
-  include SessionHelpers
+RSpec.configure do |config|
+  # ## Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
 
-  RSpec.configure do |config|
-    # ## Mock Framework
-    #
-    # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-    #
-    # config.mock_with :mocha
-    # config.mock_with :flexmock
-    # config.mock_with :rr
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-    # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-    # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  # config.use_transactional_fixtures = true
 
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    # instead of true.
-    # config.use_transactional_fixtures = true
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = false
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.filter_run_excluding :slow => true
+  config.run_all_when_everything_filtered = true
 
-    # If true, the base class of anonymous controllers will be inferred
-    # automatically. This will be the default behavior in future versions of
-    # rspec-rails.
-    config.infer_base_class_for_anonymous_controllers = false
-    config.treat_symbols_as_metadata_keys_with_true_values = true
-    config.filter_run :focus => true
-    config.filter_run_excluding :slow => true
-    config.run_all_when_everything_filtered = true
+  Capybara.javascript_driver = :webkit
+  Capybara.default_wait_time = 3
 
-    Capybara.javascript_driver = :webkit
-    Capybara.default_wait_time = 3
+  config.include FactoryGirl::Syntax::Methods
 
-    config.include FactoryGirl::Syntax::Methods
-
-    require 'database_cleaner'
-    config.before( :suite ) do
-      DatabaseCleaner.strategy = :truncation
-      DatabaseCleaner.orm = "mongoid"
-    end
-
-    config.before( :each ) do
-      DatabaseCleaner.clean
-    end
+  require 'database_cleaner'
+  config.before( :suite ) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = "mongoid"
   end
-end
 
-Spork.each_run do
-  # This code will be run each time you run your specs.
+  config.before( :each ) do
+    DatabaseCleaner.clean
+  end
 end
